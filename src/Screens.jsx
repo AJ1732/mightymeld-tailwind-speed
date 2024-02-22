a5297c056de2b2a24b88bc396039fa0acb78bbea
AJ1732 <ejemeniboi@gmail.com>
2024-02-22 23:35:52 +0100

    updated styled

--- src/Screens.jsx
+++ src/Screens.jsx
@@ -21,12 +21,12 @@ export function StartScreen({ start }) {
     <div className="w-full">
       <div className="flex flex-col justify-center items-center h-80 w-80 pb-6 bg-pink-50 text-pink-500 rounded-lg mt-16 m-auto gap-6 ">
         <h1 className="text-3xl font-bold">Memory</h1>
-        <p className="font-semibold">
+        <p className="font-medium">
           Flip over tiles looking for pairs
         </p>
         <button
           onClick={start}
-          className="bg-gradient-to-b from-pink-400 to-pink-600 text-white py-1 px-10 mt-4 rounded-full font-medium text-xl drop-shadow-lg"
+          className="bg-gradient-to-b from-pink-400 to-pink-600 text-white py-1 px-10 mt-4 rounded-full font-medium text-xl drop-shadow-2xl focus:from-pink-500 focus:to-pink-600 focus:scale-[0.99] focus:drop-shadow-none"
         >
           Play
         </button>
@@ -119,12 +119,12 @@ export function PlayScreen({ end }) {
   return (
     <>
       <div className="p-10 flex flex-col gap-12">
-        <div className="flex justify-center items-center gap-2 text-lg">
-          <p className="text-indigo-500 font-semibold tracking-tight">
+        <div className="flex justify-center items-center gap-2 text-lg font-medium">
+          <p className="text-indigo-500">
             Tries
           </p>
 
-          <p className="px-2 rounded text-indigo-600 bg-indigo-200 font-semibold">
+          <p className="px-2 rounded text-indigo-600 bg-indigo-200">
             {tryCount}
           </p>
         </div>
